19652d0d8e08a6031923bc907e7b03b03351ed53
月落星河Tsukistar <tsukistar@tsukistar.fun>
2024-03-18 11:21:27 +0800

    Update Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,7 +2,7 @@
 <template>
     <div>
         Made with ♥ by <a href="https://github.com/Tsukistar" target="_blank" rel="noopener">月落星河Tsukistar</a> 
-        ©2024. All Rights Reserved.
+        ©2024. All Rights Reserved.<a href="https://icp.gov.moe/?keyword=20244042" target="_blank">萌ICP备20244042号</a>
     </div>
 </template>
 
